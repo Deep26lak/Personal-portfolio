a25712212a8fedfefae6af064dda14be0504c9de
Deepak lak <dkumar17071@gmail.com>
2023-06-27 22:47:23 +0530

    adding javascript to files

---
+++ script.js
@@ -0,0 +1,36 @@
+const menubtn =document.querySelector("#menu__btn")
+const closebtn =document.querySelector("#close__btn")
+
+const menuItem=document.querySelector("nav .container ul")
+
+// menu items
+menubtn.addEventListener('click',()=>{
+    menuItem.style.left = "0"
+    menubtn.style.display="none"
+    closebtn.style.display ="block";
+})
+
+closebtn.addEventListener("click",()=>{
+    menuItem.style.left = "-50%"
+
+    menubtn.style.display="block"
+    closebtn.style.display ="none";
+})
+
+const navItem=document.querySelectorAll("li")
+const changeActive=()=>{
+    navItem.forEach(item=>{
+    const link = item.querySelector("a")
+    link.classList.remove('active')
+    })
+}
+
+
+
+navItem.forEach(item=>{
+    const link = item.querySelector("a")
+    link.addEventListener("click",()=>{
+        changeActive()
+        link.classList.add('active')
+    })
+})
