2256279b9d1e5f40a4e77528699a6bb7c4ad5243
Deepak lak <dkumar17071@gmail.com>
2023-07-03 00:20:06 +0530

    adding dark mode

--- script.js
+++ script.js
@@ -60,3 +60,15 @@ skillitems.forEach(skill=>{
 window.addEventListener("scroll",()=>{
     document.querySelector("nav").classList.toggle("add__shadow",window.scrollY>0)
 })
+
+
+const themeToggler = document.querySelector(".theme-toggler")
+
+
+
+themeToggler.addEventListener("click",(e)=>{
+
+    document.body.classList.toggle("dark-theme-variables")
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle('active')
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle('active')
+})
