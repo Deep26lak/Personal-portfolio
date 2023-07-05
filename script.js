d9d566b507ab2435be382c3665e69b17977b2e93
Deepak_lak <126681597+Deep26lak@users.noreply.github.com>
2023-07-05 23:18:06 +0530

    making some changes about spelings

--- script.js
+++ script.js
@@ -78,9 +78,9 @@ themeToggler.addEventListener("click",(e)=>{
 
 
 let typed = new Typed(".typing",{
- strings: ["Front-end Devoloper" , "UI/UX Designer" ,"Javascript Devoloper" , "React JS devoloper"],
- typeSpeed:150,
- backSpeed:60,
+ strings: ["Front-end Developer" , "UI/UX Designer" ,"Javascript Developer" , "React JS developer"],
+ typeSpeed:200,
+ backSpeed:100,
  loop:true,
 
 })
